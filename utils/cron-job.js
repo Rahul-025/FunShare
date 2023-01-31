118d1fb0b451f673b05d9e9a565e72e8148e5d93
Rahul Mahato <mahatorahul025@gmail.com>
2023-01-31 20:36:15 +0530

    changed some footer urls and changed the  smtp service

--- utils/cron-job.js
+++ utils/cron-job.js
@@ -1,27 +1,5 @@
 const FileModel = require("../models/file-model");
 
-// const cronJob = async () => {
-//   const pastDate = new Date(Date.now() - 8 * 60 * 60);
-//   const files = await FileModel.find({ createdAt: { $lte: pastDate } });
-
-//   const len = files.length;
-//   if (!len) {
-//     return;
-//   }
-//   console.log(`Removing ${len} files...`);
-//   for (const file of files) {
-//     try {
-//       fs.unlinkSync(file.path);
-//       await file.remove();
-//       console.log(`successfully removed ${file.name}`);
-//     } catch (error) {
-//       console.log(`Error removing file: ${file.name} - ${err.message}`);
-//     }
-//   }
-
-//   console.log(`successfully removed all files`);
-// };
-
 const cronJob = () => {
   const pastDate = new Date(Date.now() - 8 * 60 * 60);
   const files = FileModel.find({ createdAt: { $lte: pastDate } })
